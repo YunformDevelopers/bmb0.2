6660ac2ca89435bf9813ebff7f84f4fdca03518c
srtplzx <404764607@qq.com>
2014-07-19 21:30:42 +0800

    finally

--- js/create.js
+++ js/create.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-=======
 	function onshow(term){
 	  $(this).css("border-color","#ccc");
 	  term.css("box-shadow","0px 5px 5px rgba(1,1,1,0.2)");
@@ -14,9 +12,8 @@ $(document).ready(function(){
 	}
 	$("#q-del").click(function(){
 		$(this).parent().slideUp(200);
-	
+
 	});
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
     $("#tool-construct-container li").draggable({
         appendTO: "parent",
         cancel: "a.ui-icon",
@@ -33,65 +30,37 @@ $(document).ready(function(){
             if ($(ui.draggable).attr("id") == "personality") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q1 q-field-before' onmousemove='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'>          <div class='q-title'><textarea type='text' name='q1-title' rows='1' class='title edit' >姓名</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='text' name='q1-body1' class='body no-edit'/></div></div></div></li>")
-=======
                     .append("<li><div id='q' class='q1 q-field-before' onmouseover='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'>          <div class='q-title'><textarea type='text' name='q1-title' rows='1' class='title edit' >姓名</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='text' name='q1-body1' class='body no-edit'/></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             }
             else if ($(ui.draggable).attr("id") == "singleline") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q1 q-field-before' onmousemove='onshow(this)' onmouseout='offshow(this)'> <div class='q-number'> <span>Q</span> </div> <div class='q-whole'> <div class='q-title'> <textarea type='text' name='q1-title' rows='1' class='title edit' >姓名</textarea> </div> <div id='alt' class='q-alternative'> <a onclick='changeState(this)'>改为选答</a> </div> <div class='q-body'> <input type='text' name='q1-body1' class='body no-edit'/> </div> </div> </div></li>")
-=======
                     .append("<li><div id='q' class='q1 q-field-before' onmouseover='onshow(this)' onmouseout='offshow(this)'> <div class='q-number'> <span>Q</span> </div> <div class='q-whole'> <div class='q-title'> <textarea type='text' name='q1-title' rows='1' class='title edit' >姓名</textarea> </div> <div id='alt' class='q-alternative'> <a onclick='changeState(this)'>改为选答</a> </div> <div class='q-body'> <input type='text' name='q1-body1' class='body no-edit'/></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             }
             else if ($(ui.draggable).attr("id") == "multiline") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q2 q-field-before free-multiline' onmousemove='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q2-title' rows='1' class='title edit' >请问你的理想是什么？</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><textarea name='q2-body1' class='body no-edit'></textarea></div></div></div></li>")
-=======
                     .append("<li><div id='q' class='q2 q-field-before free-multiline' onmouseover='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q2-title' rows='1' class='title edit' >请问你的理想是什么？</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><textarea name='q2-body1' class='body no-edit'></textarea></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             }
             else if ($(ui.draggable).attr("id") == "file") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q6 q-field-before free-file' onmousemove='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q6-title' rows='1' class='title edit' >请上传你的个人作品</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='file' /></div></div></div></li>")
-=======
                     .append("<li><div id='q' class='q6 q-field-before free-file' onmouseover='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q6-title' rows='1' class='title edit' >请上传你的个人作品</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='file' /></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             }
             else if ($(ui.draggable).attr("id") == "personalphoto") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q5 q-field-before free-personalphoto' onmousemove='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q5-title' rows='1' class='title edit' >请上交你的个人照片</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='file' /></div></div></div></li>")
-=======
                     .append("<li><div id='q' class='q5 q-field-before free-personalphoto' onmouseover='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q5-title' rows='1' class='title edit' >请上交你的个人照片</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='file' /></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             }
             else if ($(ui.draggable).attr("id") == "singlechoice") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q4 q-field-before free-singlechoice' onmousemove='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q4-title' rows='1' class='title edit' >请问年级是？</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='radio' /><input type='text' name='q4-body1' class='body edit' value='大一'/><input type='radio' /><input type='text' name='q4-body2' class='body edit' value='大二'/><input type='radio' /><input type='text' name='q4-body3' class='body edit' value='大三'/><br /><input type='radio' /><input type='text' name='q4-body3' class='body edit' value='点击这里添加选项'/></div></div></div></li>")
-=======
                     .append("<li><div id='q' class='q4 q-field-before free-singlechoice' onmouseover='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div><div class='q-whole'><div class='q-title'><textarea type='text' name='q4-title' rows='1' class='title edit' >请问年级是？</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='radio' /><input type='text' name='q4-body1' class='body edit' value='大一'/><input type='radio' /><input type='text' name='q4-body2' class='body edit' value='大二'/><input type='radio' /><input type='text' name='q4-body3' class='body edit' value='大三'/><br /><input type='radio' /><input type='text' name='q4-body3' class='body edit' value='点击这里添加选项'/></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             }
             else if ($(ui.draggable).attr("id") == "multichoice") {
                 $(this)
                     .find("#form-body")
-<<<<<<< HEAD
-                    .append("<li><div id='q' class='q3 q-field-before free-multichoice' onmousemove='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div> <div class='q-whole'><div class='q-title'><textarea type='text' name='q3-title' rows='1' class='title edit' >请问你的志愿是哪几个？</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='checkbox' /><input type='text' name='q3-body1' class='body edit' value='项目部'/><input type='checkbox' /><input type='text' name='q3-body2' class='body edit' value='外联部'/><input type='checkbox' /><input type='text' name='q3-body3' class='body edit' value='技术部'/><br /><input type='checkbox' /><input type='text' name='q3-body3' class='body edit' value='点击这里添加选项'/></div></div></div></li>")            }
-=======
                     .append("<li><div id='q' class='q3 q-field-before free-multichoice' onmouseover='onshow(this)' onmouseout='offshow(this)'><div class='q-number'><span>Q</span></div> <div class='q-whole'><div class='q-title'><textarea type='text' name='q3-title' rows='1' class='title edit' >请问你的志愿是哪几个？</textarea></div><div id='alt' class='q-alternative'><a onclick='changeState(this)'>改为选答</a></div><div class='q-body'><input type='checkbox' /><input type='text' name='q3-body1' class='body edit' value='项目部'/><input type='checkbox' /><input type='text' name='q3-body2' class='body edit' value='外联部'/><input type='checkbox' /><input type='text' name='q3-body3' class='body edit' value='技术部'/><br /><input type='checkbox' /><input type='text' name='q3-body3' class='body edit' value='点击这里添加选项'/></div></div><a href='javascript:;' id='q-del' style='display:none;' class='q-del' title='删除'></a></div></li>")            }
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
             else if ($(ui.draggable).attr("id") == "rader") {
                 $(this)
                     .find("#form-body")
@@ -104,22 +73,5 @@ $(document).ready(function(){
         items: "li",
 		axis:"y"
     });
-<<<<<<< HEAD
-	function onshow(term){
-	  $(term).css("border-color","#ccc");
-	  $(term).css("box-shadow","0px 5px 5px rgba(1,1,1,0.2)");
-	  $("#q-del").show();
-	}
-	function offshow(term){
-	  $(term).css("border-color","rgba(255,255,255,0)");
-	  $(term).css("box-shadow","0px 0px 0px rgba(1,1,1,0)");
-	  $("#q-del").hide();
-	}
-	$("#q-del").click(function(){
-		$(this).parent().slideUp(200);
-	
-	});
-=======
->>>>>>> f71b439cf62b5546a7bd20a2c308d140e91f81e1
-	
+
 });
