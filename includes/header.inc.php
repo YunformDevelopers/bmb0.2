541b5879e18bd1ea8e99506a6d3ec851dbbc1f61
ZeroLu <iamzerolu@gmail.com>
2014-08-07 02:02:55 +0800

    优化了很多css代码，针对高分辨率屏幕增加了@2x的图片，上次commit还有部分移动端问题待解决

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -21,40 +21,40 @@ if(isset($_POST['username'])&&isset($_POST['password'])){
 ?>
 <div id='header'>
     <ul class="header-list clear-float">
-    	<li class="head-item index logo left">
+    	<li class="head-item logo left">
         	<a class="head-link" href='index.php' title="报名吧首页" >
-            	<span class="head-name">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            	<span class="head-name">首页</span>
             </a>
         </li>
-		<li class="head-item more right">
+		<li class="head-item nav more right">
         	<a class="head-link">
             	<span class="head-name">更多</span>
             </a>
         </li>
-        <li class="head-item personal right active">
+        <li class="head-item nav personal right">
         	<a class="head-link" href='personal.php' >
             	<span class="head-name">我的</span>
             </a>
         </li>
-        <li class="head-item create right">
+        <li class="head-item nav create right">
         	<a class="head-link" href='create.php' >
             	<span class="head-name">创建</span>
             </a>
         </li>
-        <li class="head-item search right">
-        	
+        <li class="head-item search-input right">	
         	<a class="head-link">
+				<span class="head-name">搜索</span>
                 <form id="search" action="#">
                     <input class="search-text" type="text"  placeholder="搜索组织/活动" name="q"></input>
                 </form>
             </a>
         </li>
     </ul>
-    <div class='search-mobile search'>
+    <!--<div class='search-mobile search'>
         <form id="search" action="#">
             <input class="search-text" type="text"  placeholder="输入你要找的组织或活动名" name="q"></input>
         </form>
-    </div>
+    </div>-->
 </div>
 <div id="tab-container">
 	<ul class="tab-list clear-float">
--- index.php
+++ index.php
@@ -216,16 +216,17 @@
                 </div>
 				</div>
             </div>
+			<div style='text-align:center;padding-top:20px;'>
+  
+			  <button onclick='mySwipe.prev()'>prev</button> 
+			  <button onclick='mySwipe.next()'>next</button>
+
+			</div>
+
 		</div>
 	</div>
 </div>
 			
-<div style='text-align:center;padding-top:20px;'>
-  
-  <button onclick='mySwipe.prev()'>prev</button> 
-  <button onclick='mySwipe.next()'>next</button>
-
-</div>
 <div class='reminder'>
 	<p>没找到想要的？试试搜索</p>
 	<div class='search'>
@@ -340,13 +341,73 @@
 		$(document).ready(function(){
 			initSectionWidth ();
 			initContentHeight ();
-		
+			initHeader();
+			
 		});
 		/* 改变窗口宽高时重新初始化宽高 */
 		window.onresize=function(){
 			initSectionWidth ();
 			initContentHeight ();
+			initHeader();
+			
+			
+			
+			
 		}
+		/* 初始化head里面的部分class */
+		function initHeader () {
+			/* 这段比较复杂 */
+			/* header里面第一个有logo和index两种状态，背景图片是不同的，一个是报名吧logo，一个是首页icon。同时index呈现的是nav的样式 */
+			/* header里面第四个是搜索，有两种状态，一种是输入框search-input，一种则是链接search。同时search呈现的是nav的样式 */
+			/* nav的背景图片位置，padding都在css文件中给出 */
+			var windowWidth = $(window).width();
+			if (windowWidth < 500) {
+				$("#header .header-list").children().eq(0).addClass("index");
+				$("#header .header-list").children().eq(0).addClass("nav");
+				$("#header .header-list").children().eq(0).removeClass("logo");
+				$("#header .header-list").children().eq(4).addClass("nav");
+				$("#header .header-list").children().eq(4).addClass("search");
+				$("#header .header-list").children().eq(4).removeClass("search-input");
+			}
+			else if (windowWidth < 750 ) {
+				$("#header .header-list").children().eq(0).addClass("logo");
+				$("#header .header-list").children().eq(0).removeClass("nav");
+				$("#header .header-list").children().eq(0).removeClass("index");
+				$("#header .header-list").children().eq(4).addClass("nav");
+				$("#header .header-list").children().eq(4).addClass("search");
+				$("#header .header-list").children().eq(4).removeClass("search-input");
+			}
+			else {
+				$("#header .header-list").children().eq(0).addClass("logo");
+				$("#header .header-list").children().eq(0).removeClass("nav");
+				$("#header .header-list").children().eq(0).removeClass("index");
+				$("#header .header-list").children().eq(4).removeClass("nav");
+				$("#header .header-list").children().eq(4).removeClass("search");
+				$("#header .header-list").children().eq(4).addClass("search-input");	
+			};
+			/*var navNameArr = new Array( "index", "search", "create", "personal", "more") ;
+			for(var i=0;i<navNameArr.length;i++) {
+				var navIth = $("#header .header-list").find("." + navNameArr[i] + " a");
+				var navIthHover = $("#header .header-list").find("." + navNameArr[i] + " a:hover");
+				var navIthActive = $("#header .header-list").find("." + navNameArr[i] + " a:active");
+				var navIthActiveClass = $("#header .header-list").find("." + navNameArr[i] + ".active a");
+				navIth.css({
+					"background-image":"url(images/header-icon/" + navNameArr[i] + ".png)"
+				});
+				navIthHover.css({
+					"background-image":"url(images/header-icon/" + navNameArr[i] + "-hover.png)",
+					"background-color":"red"
+				});
+				navIthActive.css({
+					"background-image":"url(images/header-icon/" + navNameArr[i] + "-active.png)"
+				});
+				navIthActiveClass.css({
+					"background-image":"url(images/header-icon/" + navNameArr[i] + "-active.png)"
+				});
+			}
+			*/
+		}
+		
 		/* 初始化section宽度 */
 		function initSectionWidth () {
 			var sectionWidth = $(".swipe").css("width");//获得.swipe的宽
@@ -374,8 +435,13 @@
 				startSlide: 0,
 				auto: 300000,
 				continuous: true,
-			disableScroll: true,
-			stopPropagation: true,
+				disableScroll: true,
+				stopPropagation: true,
+				callback: function(){
+					var currentPos = window.mySwipe.getPos();
+					$("#tab-container ul.tab-list").children().removeClass("active");//移除已经存在的active的class
+					$("#tab-container ul.tab-list").children().eq(currentPos).addClass("active");//被点击的父级元素添加active class
+				}
 		});
 
 		// with jQuery
