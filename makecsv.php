9a51ecc19b8d8cb22dc547ecd312177b04634c99
srtplzx <404764607@qq.com>
2014-10-20 18:56:14 +0800

    强行push

---
+++ makecsv.php
@@ -0,0 +1,49 @@
+<?php
+require 'includes/includes.inc.php';
+connect();
+// $list = array (
+// 		'aaa,bbb,ccc',
+// 		'123,456,789',
+// 		'aaa,bbb,ccc'
+// );
+$sql="select * from question where form_id='".$_GET['id']."'";
+$result=mysql_query($sql);
+$rows=mysql_fetch_assoc($result);
+$eachquestion=explode('δ', $rows['question_string']);
+$question=explode('α', $eachquestion[0]);
+$questionstring='';
+for($i=0;$i<count($eachquestion)-1;$i++){
+	$question=explode('α', $eachquestion[$i]);
+	if($i==(count($eachquestion)-2)){
+		$questionstring.=$question[0];
+	}else{
+		$questionstring.=$question[0].',';
+	}
+}
+$index=0;
+$list[$index++]= $questionstring;
+$sql="select * from answer where form_id='".$_GET['id']."'";
+$result=mysql_query($sql);
+$answerstring='';
+while($rows=mysql_fetch_assoc($result)){
+	$answerstring='';
+	$eachanswer=explode('δ', $rows['answer_string']);
+	for($i=0;$i<count($eachanswer)-2;$i++){
+		$answer=explode('γ', $eachanswer[$i]);
+		if($i==(count($eachanswer)-3)){
+			$answerstring.=$answer[0];
+		}else{
+			$answerstring.=$answer[0].',';
+		}
+	}
+	$list[$index++]=$answerstring;
+}
+$newfilename="bmb_xls_formid".$_GET['id'].'.csv';
+$fp = fopen($newfilename, 'w');
+fwrite($fp,chr(0xEF).chr(0xBB).chr(0xBF));
+  foreach ($list as $line) {
+      fputcsv($fp, explode(',', $line));
+  }
+  fclose($fp);
+  do_js_link($newfilename);
+?>
