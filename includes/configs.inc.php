a52be7cdb5e188129008e0add3786b3e1843a797
ZeroLu <iamzerolu@gmail.com>
2014-11-23 14:38:00 +0800

    尝试把config忽略掉

--- includes/configs.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_SERVER", "rdsi63aumvaqqyz.mysql.rds.aliyuncs.com");
-define("DB_USERNAME", "bmb");
-define("DB_PASSWORD", "123bmb");
-define("DB_DBNAME", "bmb");
-define("DB_CHARSET", 'utf8');
-?>
